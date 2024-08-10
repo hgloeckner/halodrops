1adfe06d50e85f0b06aff8d88c1faf5d1a1be919
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-08-10 12:27:35 -0100

    fix test_l1_path to allow new data structure

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -4,7 +4,13 @@ import os
 main_data_directory = "../sample"
 platform = "HALO"
 flightdate = "20200101"
-l1_path = os.path.join(main_data_directory, platform, flightdate, "Level_1")
+path_structure = "levels_first"
+
+if path_structure == "levels_first":
+    l1_path = os.path.join(main_data_directory, platform, "Level_1", flightdate)
+if path_structure == "flightid_first":
+    l1_path = os.path.join(main_data_directory, platform, flightdate, "Level_1")
+    
 quicklooks_path = os.path.join(main_data_directory, platform, flightdate, "Quicklooks")
 
 object = paths.Flight(main_data_directory, flightdate, platform)
